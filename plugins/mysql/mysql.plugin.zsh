7402eae25493152804cce449b5dec1060100f86e
jeryaiwei <jeryaiwei@gmail.com>
2017-02-08 09:38:06 +0800

    add

---
+++ plugins/mysql/mysql.plugin.zsh
@@ -0,0 +1,7 @@
+# commands to control local mysql-server installation
+# paths are for osx installation : path /usr/local/mysql
+
+alias mysql-start="sudo sh /usr/local/mysql/support-files/mysql.server start"
+alias mysql-stop="sudo sh /usr/local/mysql/support-files/mysql.server stop"
+alias mysql-stats="sudo sh /usr/local/mysql/support-files/mysql.server stats"
+alias mysql-restart="sudo sh /usr/local/mysql/support-files/mysql.server restart"
